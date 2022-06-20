1988ebaf20c1cd0c40dd4ae4449fb7ee1dc02e7f
ahmedhussain47 <68425053+ahmedhussain47@users.noreply.github.com>
2022-06-21 02:14:00 +0500

    javascript assignment 7

---
+++ app.js
@@ -0,0 +1,122 @@
+// Q1
+// var firstName = prompt("Enter Your First Name ");
+// var secondName = prompt("Enter Your Second Name ")
+// fullName = firstName +" "+ secondName;
+// alert("Hello Mr. "+fullName)
+
+
+// Q2
+// var modelNumber = prompt("Enter your favorite mobile model number ");
+// var modNum = modelNumber.length;
+// document.write("Your favorite Mobile model is: " + modelNumber)
+// document.write("<br>"+"Lenght of string: "+modNum);
+
+
+// Q3
+// var strNum = "Pakistani";
+// var firstChar = strNum.indexOf("n");
+// document.write("String:" + strNum);
+// document.write("<br>"+"Index of 'n': "+firstChar)
+
+
+
+//Q4
+// var lastIndexStr = "Hello World";
+// var lastIndexNum = lastIndexStr.lastIndexOf("l");
+// document.write("String:" + lastIndexStr);
+// document.write("<br>"+"Last Index of 'l': "+lastIndexNum)
+
+
+
+//Q5
+// var charStr = "Pakistani";
+// var charNum = charStr.charAt(3);
+// document.write("String:" + charStr);
+// document.write("<br>"+"Character at Index '3': "+charNum)
+
+
+// Q6
+// var firstName = prompt("Enter Your First Name ");
+// var secondName = prompt("Enter Your Second Name ")
+// document.write("Hello Mr. "+firstName +" "+ secondName)
+
+//Q7
+// var text = "Hyderabad"
+// var newText = text.replace("Hyder", "Islam");
+//  document.write("String:" + text);
+//  document.write("<br>"+"Replaced text: "+newText)
+
+
+
+//Q8
+// var message = "Ali and Sami are best friends. They play cricket and football together";
+// var newMessage = message.replaceAll("and", "&");
+//  document.write("String:" + message);
+//  document.write("<br>"+"Replaced text: "+newMessage)
+
+
+
+//Q10
+//  var smallStr = prompt("Enter Your String/Text ");
+//  var captStR = smallStr.toLocaleUpperCase();
+//  document.write("String:" + smallStr);
+//  document.write("<br>"+"String after case changing: "+captStR)
+
+
+
+//Q11
+// var inputStr = prompt('Enter a string: ');
+// var resultStr = inputStr.charAt(0).toUpperCase() + inputStr.slice(1);
+//  document.write("String:" + inputStr);
+//  document.write("<br>"+"String after case changing: "+resultStr)
+
+
+//Q12
+
+// arry1 = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var menusearch = prompt("Enter an item name to search");
+// nmenusearch = menusearch.toLowerCase();
+// if(nmenusearch=="cake")
+// {
+//         document.write("yes "+arry1[0] +" is available")
+// }
+// else if(nmenusearch=="apple pie")
+// {
+//         document.write("yes "+arry1[1] +" is available")
+// }
+// else if(nmenusearch=="cookie")
+// {
+//         document.write("yes "+arry1[2] +" is available")
+// }
+// else if(nmenusearch=="chips")
+// {
+//         document.write("yes "+arry1[3] +" is available")
+// }
+// else if(nmenusearch=="patties")
+// {
+//         document.write("yes "+arry1[3] +" is available")
+// }
+// else
+// {
+//     alert("SORRY ITEM IS NOT AVAILABLE :)")
+// }
+
+
+
+//Q16
+// var university =  "University of Karachi";
+// var ar = university.split('');
+// document.write(ar)
+
+
+//Q17
+// var lastChar = prompt("Enter your string")
+// var lastCharRes =  lastChar.charAt(lastChar.length - 1);
+// document.write("String:" + lastChar);
+// document.write("<br>"+"Last Char of "+lastChar +" is: "+lastCharRes)
+
+//Q18
+//  var initialStr = "the quick brown fox jumps over the lazy dog ";
+//  var countStr = initialStr.countOccurrences("the")
+//  document.write("String:" + initialStr);
+//  document.write("<br>"+"Answer: "+countStr)
